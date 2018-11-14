058ffd46799c6c13f2828e4a8a1ad02c8352d968
文轩 张 <zwx.atomx@outlook.com>
2018-11-14 15:39:13 +0800

    Add project files.

---
+++ Easy Five/App.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Easy_Five
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            this.Suspending += OnSuspending;
+        }
+
+        /// <summary>
+        /// Invoked when the application is launched normally by the end user.  Other entry points
+        /// will be used such as when the application is launched to open a specific file.
+        /// </summary>
+        /// <param name="e">Details about the launch request and process.</param>
+        protected override void OnLaunched(LaunchActivatedEventArgs e)
+        {
+            Launch();
+
+            Frame rootFrame = Window.Current.Content as Frame;
+
+            // Do not repeat app initialization when the Window already has content,
+            // just ensure that the window is active
+            if (rootFrame == null)
+            {
+                // Create a Frame to act as the navigation context and navigate to the first page
+                rootFrame = new Frame();
+
+                rootFrame.NavigationFailed += OnNavigationFailed;
+
+                if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
+                {
+                    //TODO: Load state from previously suspended application
+                }
+
+                // Place the frame in the current Window
+                Window.Current.Content = rootFrame;
+            }
+
+            if (e.PrelaunchActivated == false)
+            {
+                if (rootFrame.Content == null)
+                {
+                    // When the navigation stack isn't restored navigate to the first page,
+                    // configuring the new page by passing required information as a navigation
+                    // parameter
+                    rootFrame.Navigate(typeof(MainPage), e.Arguments);
+                }
+                // Ensure the current window is active
+                Window.Current.Activate();
+            }
+        }
+
+        private void Launch()
+        {
+            Windows.Graphics.Display.DisplayInformation.AutoRotationPreferences = Windows.Graphics.Display.DisplayOrientations.Landscape;
+
+            var view = Windows.UI.ViewManagement.ApplicationView.GetForCurrentView();
+
+            // 全屏
+            view.TryEnterFullScreenMode();
+
+            // active
+            view.TitleBar.BackgroundColor = Color.FromArgb(255, 45, 45, 48);
+            view.TitleBar.ForegroundColor = Colors.White;
+
+            // inactive
+            view.TitleBar.InactiveBackgroundColor = Color.FromArgb(255, 45, 45, 48);
+            view.TitleBar.InactiveForegroundColor = Colors.White;
+
+            // button
+            view.TitleBar.ButtonBackgroundColor = Color.FromArgb(255, 45, 45, 48);
+            view.TitleBar.ButtonForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonHoverBackgroundColor = Color.FromArgb(255, 75, 75, 78);
+            view.TitleBar.ButtonHoverForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonPressedBackgroundColor = Color.FromArgb(255, 75, 75, 78);
+            view.TitleBar.ButtonPressedForegroundColor = Colors.White;
+
+            view.TitleBar.ButtonInactiveBackgroundColor = Color.FromArgb(255, 45, 45, 48);
+            view.TitleBar.ButtonInactiveForegroundColor = Colors.White;
+        }
+
+        /// <summary>
+        /// Invoked when Navigation to a certain page fails
+        /// </summary>
+        /// <param name="sender">The Frame which failed navigation</param>
+        /// <param name="e">Details about the navigation failure</param>
+        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+        }
+
+        /// <summary>
+        /// Invoked when application execution is being suspended.  Application state is saved
+        /// without knowing whether the application will be terminated or resumed with the contents
+        /// of memory still intact.
+        /// </summary>
+        /// <param name="sender">The source of the suspend request.</param>
+        /// <param name="e">Details about the suspend request.</param>
+        private void OnSuspending(object sender, SuspendingEventArgs e)
+        {
+            var deferral = e.SuspendingOperation.GetDeferral();
+            //TODO: Save application state and stop any background activity
+            deferral.Complete();
+        }
+    }
+}
---
+++ Easy Five/MainPage.xaml.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Easy_Five
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private Playing playing = new Playing();
+        private bool IsCurrentBlack = true;
+
+        private void AddBlack(int index)
+        {
+            Image image = new Image();
+            BitmapImage bm = new BitmapImage();
+            bm.UriSource = new Uri("ms-appx:///Images/black.png");
+            image.Source = bm;
+            image.VerticalAlignment = VerticalAlignment.Stretch;
+            image.HorizontalAlignment = HorizontalAlignment.Stretch;
+            (board_grid.Children.ElementAt(index) as Grid).Children.Add(image);
+        }
+
+        private void AddWhite(int index)
+        {
+            Image image = new Image();
+            BitmapImage bm = new BitmapImage();
+            bm.UriSource = new Uri("ms-appx:///Images/white.png");
+            image.Source = bm;
+            image.VerticalAlignment = VerticalAlignment.Stretch;
+            image.HorizontalAlignment = HorizontalAlignment.Stretch;
+            (board_grid.Children.ElementAt(index) as Grid).Children.Add(image);
+        }
+
+        private void ClearGame()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 15; j++)
+                {
+                    if (playing.ChessBoard[i, j] != 0)
+                    {
+                        playing.ChessBoard[i, j] = 0;
+                        (board_grid.Children.ElementAt(i * 15 + j) as Grid).Children.Clear();
+                    }
+                }
+            }
+            playing.whiteCount = 0;
+            playing.blackCount = 0;
+            black_count.Text = playing.blackCount.ToString();
+            white_count.Text = playing.whiteCount.ToString();
+            IsCurrentBlack = true;
+        }
+
+        private async void Play(int row, int col)
+        {
+            int index = row * 15 + col;
+            if (IsCurrentBlack)
+            {
+                AddBlack(index);
+                playing.Cursor[0] = row;
+                playing.Cursor[1] = col;
+                playing.ChessBoard[row, col] = 1;
+                IsCurrentBlack = !IsCurrentBlack;
+
+                playing.blackCount++;
+                black_count.Text = playing.blackCount.ToString();
+                if (playing.Win(1))
+                {
+                    await new MessageDialog("Black Win").ShowAsync();
+                    ClearGame();
+                }
+            }
+            else
+            {
+                AddWhite(index);
+                playing.Cursor[0] = row;
+                playing.Cursor[1] = col;
+                playing.ChessBoard[row, col] = 2;
+                IsCurrentBlack = !IsCurrentBlack;
+
+                playing.whiteCount++;
+                white_count.Text = playing.whiteCount.ToString();
+                if (playing.Win(2))
+                {
+                    await new MessageDialog("White Win").ShowAsync();
+                    ClearGame();
+                }
+            }
+        }
+
+        private void board_PointerReleased(object sender, PointerRoutedEventArgs e)
+        {
+            Image image = sender as Image;
+            Point point = e.GetCurrentPoint(image).Position;
+            int row, col;
+            row = (int)Math.Floor(point.Y * 15 / image.ActualHeight);
+            col = (int)Math.Floor(point.X * 15 / image.ActualWidth);
+            Play(row, col);
+        }
+
+        private void Reset_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            ClearGame();
+        }
+
+        private void Return_Tapped(object sender, TappedRoutedEventArgs e)
+        {
+            int index = playing.Cursor[0] * 15 + playing.Cursor[1];
+            UIElementCollection children = (board_grid.Children.ElementAt(index) as Grid).Children;
+            if (children.Count() != 0)
+            {
+                children.Clear();
+                IsCurrentBlack = !IsCurrentBlack;
+                if (IsCurrentBlack)
+                {
+                    playing.blackCount--;
+                    black_count.Text = playing.blackCount.ToString();
+                }
+                else
+                {
+                    playing.whiteCount--;
+                    white_count.Text = playing.whiteCount.ToString();
+                }
+            }
+        }
+    }
+}
